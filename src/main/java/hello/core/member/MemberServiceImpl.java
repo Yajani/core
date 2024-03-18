ea441d9f75ceec05975f64d0b1e995822b0613ac
최예진 <bibianabb@naver.com>
2024-03-18 16:31:18 +0900

    Feat : 관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
     //null오류 때문에 구현객체(MemoryMemberRepository)를 선택해줘야한다
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    } //생성자를 통해 멤버 리포지토리에 구현체가 뭐가 들어갈지 선택함
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,16 +1,21 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
-    private DiscountPolicy discountPolicy; //인터페이스에만 의존하도록 변경
+    private final DiscountPolicy discountPolicy; //인터페이스에만 의존하도록 변경
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy, MemberRepository memberRepository1, DiscountPolicy discountPolicy1) {
+
+        this.memberRepository = memberRepository1;
+        this.discountPolicy = discountPolicy1;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);

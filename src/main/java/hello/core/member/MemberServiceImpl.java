999b54652ee7f98fd3148771b775d508a67ebbc6
최예진 <bibianabb@naver.com>
2024-03-20 16:21:43 +0900

    Feat : 컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Component("memberService2") //Bean이름 직접 지정하는 방법
 public class MemberServiceImpl implements MemberService{
     //null오류 때문에 구현객체(MemoryMemberRepository)를 선택해줘야한다
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     } //생성자를 통해 멤버 리포지토리에 구현체가 뭐가 들어갈지 선택함

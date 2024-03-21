dbd7d254fe529ba72e5a2c1871463869a960739d
최예진 <bibianabb@naver.com>
2024-03-21 15:04:31 +0900

    Feat : @RequiredArgsConstructor 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,14 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //생성자 자동생성(final을 붙인거에 생성자 생성해줌!!!)
 public class OrderServiceImpl implements OrderService{
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy; //인터 페이스 에만 의존 하도록 변경
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy; //인터 페이스 에만 의존 하도록 변경
 
 //    @Autowired(required = false) //선택적, 주입할 대상이 없어도 동작하게 하려면
 //    public void setMemberRepository(MemberRepository memberRepository) {
@@ -22,17 +24,17 @@ public class OrderServiceImpl implements OrderService{
 //        this.discountPolicy = discountPolicy;
 //    }
 
-    @Autowired //생성자가 하나면 Autowired생략가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    } //생성자를 통해서 할당
+//    @Autowired //생성자가 하나면 Autowired생략가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    } //생성자를 통해서 할당
 
-    @Autowired
-    public void init(MemberRepository memberRepository,DiscountPolicy discountPolicy){ //일반 메서드 주입
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public void init(MemberRepository memberRepository,DiscountPolicy discountPolicy){ //일반 메서드 주입
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

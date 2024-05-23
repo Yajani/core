58a2586cac2b9b7beb3d95782f6e34a9bab1d68f
예진초이 <154849422+yejiniChoi@users.noreply.github.com>
2024-05-24 00:00:00 +0900

    Bean등록 수정

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -16,21 +16,21 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; //인터 페이스 에만 의존 하도록 변경
 
-    @Autowired(required = false) //선택적, 주입할 대상이 없어도 동작하게 하려면
-    public void setMemberRepository(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+//    @Autowired(required = false) //선택적, 주입할 대상이 없어도 동작하게 하려면
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) { //수정자 주입
+//        this.discountPolicy = discountPolicy;
+//    }
 
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) { //수정자 주입
-        this.discountPolicy = discountPolicy;
-    }
-
-    @Autowired //생성자가 하나면 Autowired생략가능
-    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    } //생성자를 통해서 할당
+//    @Autowired //생성자가 하나면 Autowired생략가능
+//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    } //생성자를 통해서 할당
 
     @Autowired //생성자가 하나면 Autowired생략가능
     public OrderServiceImpl(MemberRepository memberRepository,@MainDiscountPolicy DiscountPolicy discountPolicy) {
